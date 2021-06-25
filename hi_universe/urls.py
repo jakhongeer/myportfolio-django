bd0f53d95d125133333d187910af5b7343360f70
Jahongir <jahongirnagmatov@gmail.com>
2021-06-25 12:47:08 +0500

    remove hi_universe app

--- hi_universe/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from hi_universe import views
-
-urlpatterns = [
-    path('', views.hi_universe, name='hi_universe'),
-]
--- hi_universe/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def hi_universe(request):
-    return render(request, "hi_universe.html", {})
