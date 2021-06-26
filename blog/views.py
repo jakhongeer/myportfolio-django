978ed2933a050162df359cd1cf7f0bf7b4cc2628
johndev <jahongirnagmatov@gmail.com>
2021-06-26 17:57:42 +0500

    debug path of blog in nav and blog.views

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from blog.forms import CommentForm
 # Create your views here.
 
 def blog_index(request):
-    posts = Post.object.all().order_by('-created_on')
+    posts = Post.objects.all().order_by('-created_on')
     context = {
         "posts" : posts,
     }
